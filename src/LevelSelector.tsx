f15498cf225f41fbe5c195ec9ae10941fd0f473a
Raphael Haddad <raphael@haddad.id.au>
2018-07-06 10:32:51 +1000

    Add selection of chart

--- src/LevelSelector.tsx
+++ src/LevelSelector.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react';
 class LevelSelector extends React.Component {
-
     public render() {
         return (
-        <form>
+        <form onSubmit={this.handleSubmit}>
             <label>
-              Name:
-              <input type="text" name="name" />
+              Chart:
             </label>
+                <select name="name">
+                    <option>Chart 1</option>
+                    <option>Chart 2</option>
+                    <option>Chart 3</option>
+                    <option>Chart 4</option>
+                    <option>Chart 5</option>
+                    <option>Chart 6</option>
+                </select>
             <input type="submit" value="Submit" />
         </form>
         )
+    }
+
+    private handleSubmit(): void {
+        /* tslint:disable */
+        console.log("hi");
+        /* tslint:enable */
     }
 }
 
