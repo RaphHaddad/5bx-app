f39ab1bc1998b576215e993540cebce7bf262ca6
Raphael Haddad <raphael@haddad.id.au>
2018-07-11 15:38:26 +1000

    Add chart 2

--- src/Exercise.tsx
+++ src/Exercise.tsx
@@ -24,6 +24,11 @@ class Exercise extends React.Component<ExerciseProps, ExerciseState> {
                                       new Level("C-", [6, 7, 8, 4, 205]), new Level("C", [7, 8, 10, 5, 235]), new Level("C+", [8, 9, 12, 6, 260]),
                                       new Level("B-", [10, 11, 13, 7, 280]), new Level("B", [12, 12, 14, 8, 305]), new Level("B+", [14, 13, 15, 9, 320]),
                                       new Level("A-", [16, 15, 16, 11, 335]), new Level("A", [18, 17, 17, 12, 375]), new Level("A+", [20, 18, 18, 13, 100]),
+                                    ]),
+                        new Chart(2, [new Level("D-", [14, 10, 13, 9, 335]), new Level("D", [15, 11, 14, 10, 360]), new Level("D+", [16, 12, 15, 11, 380]),
+                                      new Level("C-", [18, 13, 17, 12, 395]), new Level("C", [19, 14, 19, 13, 410]), new Level("C+", [20, 15, 21, 14, 425]),
+                                      new Level("B-", [22, 16, 23, 15, 440]), new Level("B", [24, 17, 25, 16, 455]), new Level("B+", [26, 18, 27, 17, 455]),
+                                      new Level("A-", [28, 20, 29, 18, 470]), new Level("A", [29, 21, 31, 19, 485]), new Level("A+", [30, 23, 33, 20, 500]),
                                     ])];
 
         const level = charts.filter(chart => chart.chartNumber === this.props.chart)[0].levels
