2b667e8249b569a52fba42495f5af2babb922690
Raphael Haddad <raphael@haddad.id.au>
2018-07-11 13:56:42 +1000

    Add exercise component

--- src/Exercise.tsx
+++ src/Exercise.tsx
@@ -27,17 +27,17 @@ class Exercise extends React.Component<ExerciseProps, ExerciseState> {
                                              new Level("C-", 6, 7, 8, 4, 205), new Level("D", 7, 8, 10, 5, 235), new Level("D+", 8, 9, 12, 6, 260),
                                              new Level("B-", 10, 11, 13, 7, 280), new Level("B", 12, 12, 14, 8, 305), new Level("B+", 14, 13, 15, 9, 320)])];
 
-        let chart = charts.filter((chart) => chart.chartNumber === this.props.chart)[0].levelReps;
+        let levels = charts.filter((chart) => chart.chartNumber === this.props.chart)[0].levels;
     }
 }
 
 class Chart {
-    constructor(public chartNumber: number, public levelReps: Level[]) {
+    constructor(public chartNumber: number, public levels: Level[]) {
     }
 }
 
 class Level {
-    constructor(public level: string, public exercise1Reps: number, public exercise2Reps: number, public exercise3Reps: number, public exercise4Reps: number, public exercise5Reps: number) {
+    constructor(public levels: string, public exercise1Reps: number, public exercise2Reps: number, public exercise3Reps: number, public exercise4Reps: number, public exercise5Reps: number) {
         
     }
 }
